74b3d4319012c7c51fac79b5243f3d27c925f376
Sadia Malik <sadiaabida156@gmail.com>
2023-06-11 16:06:42 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import Login from './Component/Login'
+import Eventhandling from './Component/Eventhandling';
+
+
+
+function App(){
+  return(
+    <div>
+      
+      {<Eventhandling/>}
+    
+      {/* {<Login/>} */}
+
+      
+    </div>
+  );
+}
+export default App;
+
